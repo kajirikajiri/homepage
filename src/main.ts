60fd87d4fa6c5c260ff8761012bb81b968aaee63
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-07-11 19:23:28 +0900

    Quick update

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueMarkdown from 'vue-markdown'
 
 Vue.config.productionTip = false
+
+Vue.component('vue-markdown', VueMarkdown)
 
 new Vue({
   router,
