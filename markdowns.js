2cde7a414340e6cebf505b7d9b40c74c807865f8
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-07-12 20:23:37 +0900

    test9

--- markdowns.js
+++ markdowns.js
@@ -1,10 +1,5 @@
 export const markdowns = [
   {
-    name: 'sample_copy.md',
-    birth: 'Sun Jul 12 20:22:45 2020	',
-    modify: '2020-07-12 20:22:50.861000000 +0900',
-  },
-  {
     name: 'sample.md',
     birth: 'Sun Jul 12 20:03:30 2020	',
     modify: '2020-07-12 20:03:53.575529300 +0900',
