66c0ce9a777e9542086b10839b6c2019d58ad952
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-07-12 20:04:57 +0900

    fix

--- markdowns.js
+++ markdowns.js
@@ -1,4 +1,17 @@
 export const markdowns = [
-  {name: 'hoge.md', birth: 'Sat Jul 11 19:29:14 2020	', modify: '2020-07-12 17:04:18.875529300 +0900'},
-  {name: 'copy.md', birth: 'Sun Jul 12 15:55:34 2020	', modify: '2020-07-12 16:41:37.945529300 +0900'},
+  {
+    name: 'sample.md',
+    birth: 'Sun Jul 12 20:03:30 2020	',
+    modify: '2020-07-12 20:03:53.575529300 +0900',
+  },
+  {
+    name: 'hoge.md',
+    birth: 'Sat Jul 11 19:29:14 2020	',
+    modify: '2020-07-12 17:04:18.875529300 +0900',
+  },
+  {
+    name: 'copy.md',
+    birth: 'Sun Jul 12 15:55:34 2020	',
+    modify: '2020-07-12 16:41:37.945529300 +0900',
+  },
 ]
