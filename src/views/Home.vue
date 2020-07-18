552a6af520186330721552e57d201e3280bc8368
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-07-18 14:45:59 +0900

    Quick update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,23 @@
       <div
         @click="clickUpdateSort()"
         class="rotate"
-        :style="styleSortUpdateButton"
+        :style="updateOrderStyle.button"
       >
-        <span poyonTargetButtonUpdate class="poyon">⬆</span>
-        <span :style="wordStyleSortUpdate">Update</span>
+        <span
+          poyonTargetButtonUpdate
+          class="poyon"
+          :style="updateOrderArrowStyle"
+          >⬆</span
+        >
+        <span :style="updateOrderStyle.word">Update</span>
       </div>
       <div
         @click="clickCreateSort()"
         class="rotate"
-        :style="styleSortCreateButton"
+        :style="createOrderStyle.button"
       >
-        <span poyonTargetButtonCreate>⬆</span>
-        <span :style="wordStyleSortCreate">Create</span>
+        <span poyonTargetButtonCreate :style="createOrderArrowStyle">⬆</span>
+        <span :style="createOrderStyle.word">Create</span>
       </div>
     </div>
     <article-list :markdowns="computedMarkdowns" />
@@ -26,6 +31,19 @@
 import { markdowns } from '../../markdowns.js'
 import ArticleList from '@/components/ArticleList'
 
+const buttonStyle = {
+  active: 'background:#6200ee;color:#fff;font-size:20px;font-weight:bold;',
+  inActive:
+    'background:lightgrey;color:grey;font-size:14px;font-weight:normal;',
+}
+const wordStyle = {
+  active: 'font-size:14px;font-weight:bold',
+  inActive: 'font-size:12px;font-weight:bold;',
+}
+const arrowStyle = {
+  active: 'transform:rotate(0deg);',
+  inActive: 'transform:rotate(180deg);',
+}
 export default {
   components: {
     ArticleList,
@@ -36,13 +54,49 @@ export default {
       updateSortFlag: false,
       createSortFlag: false,
       currentSort: 'update',
-      styleSortUpdateButton: 'background:#6200ee;color:#fff;',
-      styleSortCreateButton: 'background:lightgrey;color:grey;',
-      wordStyleSortUpdate: 'font-size:12px;font-weight:bold;',
-      wordStyleSortCreate: 'font-size:12px;font-weight:bold;',
     }
   },
   computed: {
+    createOrderStyle() {
+      if (this.currentSort === 'create') {
+        return {
+          button: buttonStyle.active,
+          word: wordStyle.active,
+        }
+      } else {
+        return {
+          button: buttonStyle.inActive,
+          word: wordStyle.inActive,
+        }
+      }
+    },
+    updateOrderStyle() {
+      if (this.currentSort === 'update') {
+        return {
+          button: buttonStyle.active,
+          word: wordStyle.active,
+        }
+      } else {
+        return {
+          button: buttonStyle.inActive,
+          word: wordStyle.inActive,
+        }
+      }
+    },
+    createOrderArrowStyle() {
+      if (this.createSortFlag) {
+        return arrowStyle.inActive
+      } else {
+        return arrowStyle.active
+      }
+    },
+    updateOrderArrowStyle() {
+      if (this.updateSortFlag) {
+        return arrowStyle.inActive
+      } else {
+        return arrowStyle.active
+      }
+    },
     computedMarkdowns() {
       const result = [...this.markdowns].sort((a, b) => {
         if (this.currentSort === 'create') {
@@ -64,60 +118,18 @@ export default {
   },
   methods: {
     clickUpdateSort() {
-      this.poyon('poyonTargetButtonUpdate')
-      console.log(this.currentSort, this.updateSortFlag)
-      if (this.currentSort === 'update' && this.updateSortFlag) {
-        this.updateSortFlag = !this.updateSortFlag
-        this.styleSortUpdateButton =
-          'transform:rotate(0deg);background:#3700b3;transition: transform 1s;color:#fff;'
-        this.wordStyleSortUpdate =
-          'font-weight:bold;font-size:12px;transform:rotate(0deg);transition: transform 0.5s 1s;'
-      } else if (this.currentSort === 'update' && !this.updateSortFlag) {
-        this.updateSortFlag = !this.updateSortFlag
-        this.styleSortUpdateButton =
-          'transform:rotate(180deg);background:#6200ee;transition: transform 1s;color:#fff;'
-        this.wordStyleSortUpdate =
-          'font-weight:bold;font-size:12px;transform:rotate(180deg);transition: transform 0.5s 1s;'
-      } else if (this.currentSort === 'create' && this.updateSortFlag) {
+      if (this.currentSort === 'create') {
         this.currentSort = 'update'
-        this.styleSortCreateButton = 'background:lightgrey;color:grey;'
-        this.styleSortUpdateButton = 'color:#fff;background:#3700b3;'
-      } else if (this.currentSort === 'create' && !this.updateSortFlag) {
-        this.currentSort = 'update'
-        this.styleSortCreateButton = 'background:lightgrey;color:grey;'
-        this.styleSortUpdateButton = 'color:#fff;background:#6200ee;'
+      } else {
+        this.updateSortFlag = !this.updateSortFlag
       }
     },
     clickCreateSort() {
-      this.poyon('poyonTargetButtonCreate')
-      if (this.currentSort === 'create' && this.createSortFlag) {
-        this.createSortFlag = !this.createSortFlag
-        this.styleSortCreateButton =
-          'transform:rotate(0deg);background:#3700b3;transition: transform 1s;color:#fff;'
-        this.wordStyleSortCreate =
-          'font-weight:bold;font-size:12px;transform:rotate(0deg);transition: transform 0.5s 1s;'
-      } else if (this.currentSort === 'create' && !this.createSortFlag) {
-        this.createSortFlag = !this.createSortFlag
-        this.styleSortCreateButton =
-          'transform:rotate(180deg);background:#6200ee;transition: transform 1s;color:#fff;'
-        this.wordStyleSortCreate =
-          'font-weight:bold;font-size:12px;transform:rotate(180deg);transition: transform 0.5s 1s;'
-      } else if (this.currentSort === 'update' && this.createSortFlag) {
+      if (this.currentSort === 'update') {
         this.currentSort = 'create'
-        this.styleSortUpdateButton = 'background:lightgrey;color:grey;'
-        this.styleSortCreateButton = 'color:#fff;background:#3700b3;'
-      } else if (this.currentSort === 'update' && !this.createSortFlag) {
-        this.currentSort = 'create'
-        this.styleSortUpdateButton = 'background:lightgrey;color:grey;'
-        this.styleSortCreateButton = 'color:#fff;background:#6200ee;'
+      } else {
+        this.createSortFlag = !this.createSortFlag
       }
-    },
-    poyon(target) {
-      const element = document.querySelector('[' + target + ']')
-      element.classList.add('poyon')
-      setTimeout(() => {
-        element.className = ''
-      }, 2000)
     },
   },
 }
@@ -134,36 +146,5 @@ export default {
   border-radius: 4px;
   margin-right: 10px;
   margin-bottom: 20px;
-}
-.poyon {
-  font-weight: bold;
-  font-size: 20px;
-  animation: poyon 0.9s linear 1s 1;
-}
-@keyframes poyon {
-  0% {
-    transform: scale(1, 1) translate(0%, 0%);
-  }
-  10% {
-    transform: scale(1.1, 0.9) translate(0%, 5%);
-  }
-  40% {
-    transform: scale(1.2, 0.8) translate(0%, 15%);
-  }
-  50% {
-    transform: scale(1, 1) translate(0%, 0%);
-  }
-  60% {
-    transform: scale(0.9, 1.2) translate(0%, -100%);
-  }
-  75% {
-    transform: scale(0.9, 1.2) translate(0%, -20%);
-  }
-  85% {
-    transform: scale(1.2, 0.8) translate(0%, 15%);
-  }
-  100% {
-    transform: scale(1, 1) translate(0%, 0%);
-  }
 }
 </style>
